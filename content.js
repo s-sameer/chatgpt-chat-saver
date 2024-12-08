44b6666c813cce4d47e7124286b38e1e6ec3bee7
Sadman Sameer <itsnotsameer@gmail.com>
2024-12-08 17:43:36 -0330

    add better user exp

--- content.js
+++ content.js
@@ -1,11 +1,15 @@
+const saveIcon = `
+    <svg viewBox="0 0 24 24" version="1.1" height="20" width="20" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#e8e8e8" stroke="#e8e8e8"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>Save</title> <g id="页面-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Education" transform="translate(-96.000000, -48.000000)" fill-rule="nonzero"> <g id="bookmark_fill" transform="translate(96.000000, 48.000000)"> <path d="M24,0 L24,24 L0,24 L0,0 L24,0 Z M12.5934901,23.257841 L12.5819402,23.2595131 L12.5108777,23.2950439 L12.4918791,23.2987469 L12.4918791,23.2987469 L12.4767152,23.2950439 L12.4056548,23.2595131 C12.3958229,23.2563662 12.3870493,23.2590235 12.3821421,23.2649074 L12.3780323,23.275831 L12.360941,23.7031097 L12.3658947,23.7234994 L12.3769048,23.7357139 L12.4804777,23.8096931 L12.4953491,23.8136134 L12.4953491,23.8136134 L12.5071152,23.8096931 L12.6106902,23.7357139 L12.6232938,23.7196733 L12.6232938,23.7196733 L12.6266527,23.7031097 L12.609561,23.275831 C12.6075724,23.2657013 12.6010112,23.2592993 12.5934901,23.257841 L12.5934901,23.257841 Z M12.8583906,23.1452862 L12.8445485,23.1473072 L12.6598443,23.2396597 L12.6498822,23.2499052 L12.6498822,23.2499052 L12.6471943,23.2611114 L12.6650943,23.6906389 L12.6699349,23.7034178 L12.6699349,23.7034178 L12.678386,23.7104931 L12.8793402,23.8032389 C12.8914285,23.8068999 12.9022333,23.8029875 12.9078286,23.7952264 L12.9118235,23.7811639 L12.8776777,23.1665331 C12.8752882,23.1545897 12.8674102,23.1470016 12.8583906,23.1452862 L12.8583906,23.1452862 Z M12.1430473,23.1473072 C12.1332178,23.1423925 12.1221763,23.1452606 12.1156365,23.1525954 L12.1099173,23.1665331 L12.0757714,23.7811639 C12.0751323,23.7926639 12.0828099,23.8018602 12.0926481,23.8045676 L12.108256,23.8032389 L12.3092106,23.7104931 L12.3186497,23.7024347 L12.3186497,23.7024347 L12.3225043,23.6906389 L12.340401,23.2611114 L12.337245,23.2485176 L12.337245,23.2485176 L12.3277531,23.2396597 L12.1430473,23.1473072 Z" id="MingCute" fill-rule="nonzero"> </path> <path d="M4,5 C4,3.34315 5.34315,2 7,2 L17,2 C18.6569,2 20,3.34315 20,5 L20,21.0284 C20,22.2485 18.6209,22.9581 17.6281,22.249 L12,18.2289 L6.37186,22.249 C5.37906,22.9581 4,22.2485 4,21.0284 L4,5 Z" id="路径" fill="#e8e8e8"> </path> </g> </g> </g> </g></svg>
+`;
+const saveIconGreen = `
+    <svg viewBox="0 0 24 24" version="1.1" height="20" width="20" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#1cba86"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>Saved</title> <g id="页面-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Education" transform="translate(-96.000000, -48.000000)" fill-rule="nonzero"> <g id="bookmark_fill" transform="translate(96.000000, 48.000000)"> <path d="M24,0 L24,24 L0,24 L0,0 L24,0 Z M12.5934901,23.257841 L12.5819402,23.2595131 L12.5108777,23.2950439 L12.4918791,23.2987469 L12.4918791,23.2987469 L12.4767152,23.2950439 L12.4056548,23.2595131 C12.3958229,23.2563662 12.3870493,23.2590235 12.3821421,23.2649074 L12.3780323,23.275831 L12.360941,23.7031097 L12.3658947,23.7234994 L12.3769048,23.7357139 L12.4804777,23.8096931 L12.4953491,23.8136134 L12.4953491,23.8136134 L12.5071152,23.8096931 L12.6106902,23.7357139 L12.6232938,23.7196733 L12.6232938,23.7196733 L12.6266527,23.7031097 L12.609561,23.275831 C12.6075724,23.2657013 12.6010112,23.2592993 12.5934901,23.257841 L12.5934901,23.257841 Z M12.8583906,23.1452862 L12.8445485,23.1473072 L12.6598443,23.2396597 L12.6498822,23.2499052 L12.6498822,23.2499052 L12.6471943,23.2611114 L12.6650943,23.6906389 L12.6699349,23.7034178 L12.6699349,23.7034178 L12.678386,23.7104931 L12.8793402,23.8032389 C12.8914285,23.8068999 12.9022333,23.8029875 12.9078286,23.7952264 L12.9118235,23.7811639 L12.8776777,23.1665331 C12.8752882,23.1545897 12.8674102,23.1470016 12.8583906,23.1452862 L12.8583906,23.1452862 Z M12.1430473,23.1473072 C12.1332178,23.1423925 12.1221763,23.1452606 12.1156365,23.1525954 L12.1099173,23.1665331 L12.0757714,23.7811639 C12.0751323,23.7926639 12.0828099,23.8018602 12.0926481,23.8045676 L12.108256,23.8032389 L12.3092106,23.7104931 L12.3186497,23.7024347 L12.3186497,23.7024347 L12.3225043,23.6906389 L12.340401,23.2611114 L12.337245,23.2485176 L12.337245,23.2485176 L12.3277531,23.2396597 L12.1430473,23.1473072 Z" id="MingCute" fill-rule="nonzero"> </path> <path d="M4,5 C4,3.34315 5.34315,2 7,2 L17,2 C18.6569,2 20,3.34315 20,5 L20,21.0284 C20,22.2485 18.6209,22.9581 17.6281,22.249 L12,18.2289 L6.37186,22.249 C5.37906,22.9581 4,22.2485 4,21.0284 L4,5 Z" id="路径" fill="#1cba86"> </path> </g> </g> </g> </g></svg>
+`;
+
 const observer = new MutationObserver(() => {
     const sidebar = document.querySelector('nav div.bg-token-sidebar-surface-primary')
-    const saveIcon = `
-        <svg viewBox="0 0 24 24" fill="none" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M9.25 3.5C7.45507 3.5 6 4.95507 6 6.75V24.75C6 25.0348 6.16133 25.2951 6.41643 25.4217C6.67153 25.5484 6.97638 25.5197 7.20329 25.3475L14 20.1914L20.7967 25.3475C21.0236 25.5197 21.3285 25.5484 21.5836 25.4217C21.8387 25.2951 22 25.0348 22 24.75V6.75C22 4.95507 20.5449 3.5 18.75 3.5H9.25Z" fill="#e7e4e4"></path> </g></svg>
-    `;
 
     if (sidebar && !document.querySelector('#saved-chats-button')) {
-        createSavedChatsButton(sidebar, saveIcon);
+        createSavedChatsButton(sidebar);
         createSavedChatsList();
         loadSavedChats();
     }
@@ -15,7 +19,7 @@ const observer = new MutationObserver(() => {
 
 observer.observe(document.body, { childList: true, subtree: true });
 
-function createSavedChatsButton(sidebar, saveIcon) {
+function createSavedChatsButton(sidebar) {
     const savedChatsButton = document.createElement('div');
     savedChatsButton.id = 'saved-chats-button';
     savedChatsButton.innerHTML = `${saveIcon} Saved Chats`;
@@ -49,7 +53,7 @@ function createSavedChatsList() {
     savedChatsButton.insertAdjacentElement('afterend', savedChatsList);
 }
 
-function createSaveIconWrapper(saveIcon) {
+function createSaveIconWrapper() {
     const saveIconWrapper = document.createElement('span');
     saveIconWrapper.innerHTML = saveIcon;
     saveIconWrapper.className = 'save-icon-wrapper';
@@ -64,16 +68,12 @@ function createSaveIconWrapper(saveIcon) {
 }
 
 function attachHoverEvents(chatItem, saveIconWrapper) {
-    const anchorTag = chatItem.querySelector('a');
-
     chatItem.addEventListener('mouseenter', () => {
         saveIconWrapper.style.display = 'block';
     });
 
     chatItem.addEventListener('mouseleave', () => {
-        if (!isChatSaved(anchorTag)) {
-            saveIconWrapper.style.display = 'none';
-        }
+        saveIconWrapper.style.display = 'none';
     });
 }
 
@@ -85,13 +85,23 @@ function attachSaveIconClickEvent(chatItem, saveIconWrapper) {
 
         if (!isSaved) {
             saveChat(chatItem, anchorTag.href, savedChatsList);
-        }
+            saveIconWrapper.innerHTML = saveIconGreen;
+        } 
     });
 }
 
 function isChatSaved(anchorTag) {
     const savedChats = JSON.parse(localStorage.getItem('savedChats')) || {};
     return anchorTag && savedChats[anchorTag.href];
+}
+
+function resetChatState(href){
+    const relativeHref = href.replace(window.location.origin, '');
+    const originalChatItem = document.querySelector(`li[data-testid^="history-item-"] a[href="${relativeHref}"]`)?.closest('li');
+    if (originalChatItem) {
+        const originalSaveIconWrapper = originalChatItem.querySelector('.save-icon-wrapper');
+        originalSaveIconWrapper.innerHTML = saveIcon; // Reset to default icon
+    }
 }
 
 function saveChat(chatItem, href, savedChatsList) {
@@ -110,11 +120,12 @@ function cloneChatItem(chatItem) {
 
 function setupClonedSaveIcon(clonedChat, href) {
     const clonedSaveIconWrapper = clonedChat.querySelector('.save-icon-wrapper');
-    clonedSaveIconWrapper.style.right = '4%';
+    clonedSaveIconWrapper.style.right = '3%';
     clonedSaveIconWrapper.addEventListener('click', (e) => {
         e.stopPropagation();
         clonedChat.remove();
         removeChatFromLocalStorage(href);
+        resetChatState(href);
     });
 }
 
@@ -136,6 +147,7 @@ function loadSavedChats() {
             e.stopPropagation();
             chatItem.remove();
             removeChatFromLocalStorage(href);
+            resetChatState(href);
         });
 
         savedChatsList.appendChild(chatItem);
@@ -148,7 +160,7 @@ function removeChatFromLocalStorage(href) {
     localStorage.setItem('savedChats', JSON.stringify(savedChats));
 }
 
-function addSaveIconsToChatItems(saveIcon) {
+function addSaveIconsToChatItems() {
     const chatItems = document.querySelectorAll('li[data-testid^="history-item-"]');
 
     chatItems.forEach((chatItem) => {
@@ -163,7 +175,7 @@ function addSaveIconsToChatItems(saveIcon) {
 
         const anchorTag = chatItem.querySelector('a');
         if (isChatSaved(anchorTag)) {
-            saveIconWrapper.style.display = 'block'; // Make the save icon visible
+            saveIconWrapper.innerHTML = saveIconGreen; 
         }
     });
 }
